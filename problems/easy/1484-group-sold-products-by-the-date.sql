0cddbcf4df3a6749b844b9c9627409c61d2e4ceb
Nihad Nahmatli <140307533+developerNihad@users.noreply.github.com>
2025-09-20 19:13:32 +0400

    Create 1484-group-sold-products-by-the-date.sql

---
+++ problems/easy/1484-group-sold-products-by-the-date.sql
@@ -0,0 +1,10 @@
+SELECT 
+    TO_CHAR(sell_date, 'YYYY-MM-DD') AS sell_date,
+    COUNT(product) AS num_sold,
+    LISTAGG(product, ',') WITHIN GROUP (ORDER BY product) AS products
+FROM (
+    SELECT DISTINCT sell_date, product
+    FROM Activities
+)
+GROUP BY sell_date
+ORDER BY sell_date;
